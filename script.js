c6fb73c879c8e7cfb9d0e7a1d3f518d89ca662e4
foyem <anifowose.adeyemi@gmail.com>
2022-03-30 02:25:31 +0100

    new changes

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web Developer", "IT Support", "UI/UX Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
